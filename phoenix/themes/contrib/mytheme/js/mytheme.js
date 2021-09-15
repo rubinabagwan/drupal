aee081bd06f616de7da715b5e99d2d2332cf711d
rubinabagwan <rubinab@cybage.com>
2021-09-15 14:27:20 +0530

    new

---
+++ phoenix/themes/contrib/mytheme/js/mytheme.js
@@ -0,0 +1,8 @@
+(function($, Drupal) {
+   Drupal.behaviors.mytheme = {
+   attach: function(context, settings) {
+       console.log(123);
+      // $('.page-header').css('background','#001');
+   }
+    };
+}) (jQuery, Drupal);
